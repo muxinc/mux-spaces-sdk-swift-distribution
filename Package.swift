6a2ad7e477e6eb6eb6f7220f5ae5c6818e794ce0
AJ Lauer Barinov <abarinov@mux.com>
2022-12-22 14:18:13 -0800

    Add binary distribution manifest for v0.2.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "MuxSpaces",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "MuxSpaces",
@@ -14,12 +14,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MuxSpaces",
-            url: "https://github.com/muxinc/mux-spaces-sdk-swift-distribution/releases/download/v0.1.0/MUXSpaces.xcframework.zip",
-            checksum: "c11ca7998507f8bccbac6d59979b29a5bd721298ddfc3c30e38b34d7d200dd24"
+            url: "https://github.com/muxinc/mux-spaces-sdk-swift-distribution/releases/download/v0.2.0/MUXSpaces.xcframework.zip",
+            checksum: "5d1f7b3442f5926384bccdef564081e95997b359d9a12d7b430021fd776bcac6"
         ),
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/muxinc/mux-spaces-sdk-swift-distribution/releases/download/v0.1.0/WebRTC.xcframework.zip",
+            url: "https://github.com/muxinc/mux-spaces-sdk-swift-distribution/releases/download/v0.2.0/WebRTC.xcframework.zip",
             checksum: "fa108bc7931719e6abae799516add0fa2944fe2f35e3a69bc825f33d72b7995a"
         ),
     ]
